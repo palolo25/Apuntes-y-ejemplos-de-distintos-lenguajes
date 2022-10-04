7a06f02e54c489229446586508ddacdfcf3ac951
pablo segura barranco <89455065+palolo25@users.noreply.github.com>
2022-10-04 18:27:36 +0200

    nuevo doc

--- Java/Practicas_y_contenido/introduccion_java/src/introduccion_java/EntradaYsalidaDeDatos.java
+++ Java/Practicas_y_contenido/introduccion_java/src/introduccion_java/EntradaYsalidaDeDatos.java
@@ -1,10 +1,10 @@
 package introduccion_java;
 
-public class hola_mundo {
+public class EntradaYsalidaDeDatos {
 
 	public static void main(String[] args) {
+		// TODO Auto-generated method stub
 		System.out.println("hola mundo");
-		
 	}
 
 }
