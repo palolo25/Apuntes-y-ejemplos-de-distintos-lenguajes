95d47bf86f88085ed3c9a525499001d2fbf393bd
pablo segura barranco <89455065+palolo25@users.noreply.github.com>
2022-10-13 18:44:33 +0200

    incre y decre

---
+++ Java/Practicas_y_contenido/operadores_de_java/src/operadores_de_java/operadores_de_incremento_decremento.java
@@ -0,0 +1,18 @@
+package operadores_de_java;
+
+public class operadores_de_incremento_decremento {
+
+    public static void main(String[] args) {
+        
+        int x = 5, y;
+        y = --x;
+        x++;// x += 1;
+        x--;// x -= 1;
+        
+        System.out.println(x);
+        System.out.println(y);
+        
+
+    }
+
+}
