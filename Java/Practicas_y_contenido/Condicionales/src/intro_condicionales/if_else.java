f66ade77d515a32657176bdb3a5a48f799055018
pablo segura barranco <89455065+palolo25@users.noreply.github.com>
2022-11-10 21:18:32 +0100

    condicionales

---
+++ Java/Practicas_y_contenido/Condicionales/src/intro_condicionales/if_else.java
@@ -0,0 +1,35 @@
+/*
+ * La sentencia if
+ * 
+ * 
+ * if(condicion){
+ * instruccion1;
+ * }
+ * else {
+ * instruccion2;
+ * }
+ * */
+package intro_condicionales;
+
+import javax.swing.JOptionPane;
+
+public class if_else {
+
+    public static void main(String[] args) {
+        
+        int numero, dato = 5 ;
+        
+        numero = Integer.parseInt(JOptionPane.showInputDialog("Escriba un numero: "));
+        
+        if (numero == dato) {
+            
+            JOptionPane.showMessageDialog(null, "El numero es 5");
+            
+        }
+        else {
+            JOptionPane.showMessageDialog(null, "El numero es diferente de 5");   
+        }
+
+    }
+
+}
