5339f925dd91c797fbc454910f424e5d5bf45046
pablo segura barranco <89455065+palolo25@users.noreply.github.com>
2022-11-10 21:51:37 +0100

    practicas1

---
+++ Java/Practicas_y_contenido/Condicionales/src/PracticasCondicionales/DeterminarMultiploDe.java
@@ -0,0 +1,22 @@
+package PracticasCondicionales;
+
+import javax.swing.JOptionPane;
+
+public class DeterminarMultiploDe {
+
+    public static void main(String[] args) {
+        
+        int numero;
+        
+        numero = Integer.parseInt(JOptionPane.showInputDialog("Escriba un numero: "));
+        
+        if(numero%10 == 0) {
+            
+            JOptionPane.showMessageDialog(null, "El numero " + numero + " es multiplo de 10");
+        }
+        else {
+            JOptionPane.showMessageDialog(null, "El numero no es multiplo de 10");
+        }
+    }
+
+}
