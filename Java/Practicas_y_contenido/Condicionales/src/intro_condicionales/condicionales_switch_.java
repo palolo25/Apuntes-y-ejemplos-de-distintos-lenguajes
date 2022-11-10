7a05e1e74eb4f4b709365cd47137a3df19528c71
pablo segura barranco <89455065+palolo25@users.noreply.github.com>
2022-11-10 21:35:49 +0100

    condicionales2

--- Java/Practicas_y_contenido/Condicionales/src/intro_condicionales/condicionales_switch_.java
+++ Java/Practicas_y_contenido/Condicionales/src/intro_condicionales/condicionales_switch_.java
@@ -1,9 +1,60 @@
+
+/*La sentecia switch:
+ * 
+ * switch(dato){
+ * 
+ * 
+ * 
+ * case 1 : instrucuones1;
+ *          break;
+ *          
+ *          
+ * case 2 : instrucuones2;
+ *          break;
+ *          
+ * case 3 : instrucuones3;
+ *          break;
+ *          
+ *          
+ * case 4 : instrucuones4;
+ *          break;
+ * }
+ * */
 package intro_condicionales;
+
+import javax.swing.JOptionPane;
 
 public class condicionales_switch_ {
 
     public static void main(String[] args) {
-        // TODO Auto-generated method stub
+       
+        
+        int dato;
+        
+        dato = Integer.parseInt(JOptionPane.showInputDialog("Escriba un numero entre 1 y 5: "));
+        
+        
+        switch(dato) {
+            
+            case 1: JOptionPane.showMessageDialog(null, "Es el numero 1");
+            break;
+            
+            case 2: JOptionPane.showMessageDialog(null, "Es el numero 2");
+            break;
+            
+            case 3: JOptionPane.showMessageDialog(null, "Es el numero 3");
+            break;
+            
+            case 4: JOptionPane.showMessageDialog(null, "Es el numero 4");
+            break;
+            
+            case 5: JOptionPane.showMessageDialog(null, "Es el numero 5");
+            break;
+            
+            default: JOptionPane.showMessageDialog(null, "Ese numero es invalido");
+            
+            
+        }
 
     }
 
