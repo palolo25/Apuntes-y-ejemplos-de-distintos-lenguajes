7fa112ac351743f3333d60044c0b70e480a8bcc0
pablo segura barranco <89455065+palolo25@users.noreply.github.com>
2023-01-13 21:45:43 +0100

    POO

---
+++ Java/Practicas_y_contenido/POO_practicas/src/Cochebarato/Vehiculo.java
@@ -0,0 +1,41 @@
+/*
+ Ejercicio 3: Construir un programa que dada una serie de vehículos caracterizados por su marca, modelo y precio, imprima las propiedades del vehículo más barato.
+Para ello, se deberán leer por
+teclado las características de cada vehiculo y crear una clase que represente a cada uno de
+ellos
+  */
+
+
+package Cochebarato;
+
+public class Vehiculo {
+	
+	private String marca;
+	private String modelo;
+	private float precio;
+	
+	
+	public Vehiculo(String marca, String modelo, float precio) {
+		super();
+		this.marca = marca;
+		this.modelo = modelo;
+		this.precio = precio;
+	}
+
+	public float getPrecio() {
+		return precio;
+	}
+
+	public void setPrecio(float precio) {
+		this.precio = precio;
+	}
+	
+	public String mostrarDatos() {
+		
+		return "Marca: "+marca+"\nModelo: "+modelo+"\nPrecio: "+precio+"\n";
+		
+	}
+	
+	
+
+}
