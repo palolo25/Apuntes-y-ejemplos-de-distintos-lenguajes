b72772b30279900f382168d56b126d09584dea1b
pablo segura barranco <89455065+palolo25@users.noreply.github.com>
2023-04-02 22:45:38 +0200

    agregacion de objetos en clase Punto

--- Python/POO/Creacion_de_objetos/Punto.py
+++ Python/POO/Creacion_de_objetos/Punto.py
@@ -1,5 +1,5 @@
 import matplotlib.pyplot as plt
-class punto:
+class Punto:
     def __init__(self,x,y):
         self.X = x
         self.Y = y
@@ -11,12 +11,21 @@ class punto:
         plt.scatter(self.X, self.Y, color='red')
         plt.show()
         
-    
 # Crear un objeto de la clase Punto con las coordenadas (3, 4)
-mi_punto = punto(3, 4)
+mi_punto1 = Punto(3, 4)
+
+# Crear otro objeto de la clase Punto con las coordenadas (-2, 7)
+mi_punto2 = Punto(-2, 7)
+
+# Crear un tercer objeto de la clase Punto con las coordenadas (0, -5)
+mi_punto3 = Punto(0, -5)
+
+# Llamar al método dibujarPunto para graficar los puntos
+mi_punto1.dibujarPunto()
+mi_punto2.dibujarPunto()
+mi_punto3.dibujarPunto()
 
 # Llamar al método mostrarPunto para imprimir las coordenadas del punto.
-mi_punto.mostrarPunto() # Salida: "El punto es( 3 , 4 )"
-
-# Llamar al método dibujarPunto para graficar el punto.
-mi_punto.dibujarPunto()
+mi_punto1.mostrarPunto() # Salida: "El punto es( 3 , 4 )"
+mi_punto2.mostrarPunto() # Salida: "El punto es( -2 , 7 )"
+mi_punto3.mostrarPunto() # Salida: "El punto es( 0 , 5 )"
