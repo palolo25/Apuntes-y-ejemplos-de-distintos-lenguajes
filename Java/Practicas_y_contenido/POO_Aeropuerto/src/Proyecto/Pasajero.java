ae02b89f421f24842e964d8a88d4462385f09dbf
pablo segura barranco <89455065+palolo25@users.noreply.github.com>
2023-08-09 11:51:40 +0200

    vuelo y pasajero

--- Java/Practicas_y_contenido/POO_Aeropuerto/src/Proyecto/Pasajero.java
+++ Java/Practicas_y_contenido/POO_Aeropuerto/src/Proyecto/Pasajero.java
@@ -2,4 +2,34 @@ package Proyecto;
 
 public class Pasajero {
 
+	private String nombre;
+	private String pasaporte;
+	private String nacionalidad;
+	
+	
+	public Pasajero(String nombre, String pasaporte, String nacionalidad) {
+		super();
+		this.nombre = nombre;
+		this.pasaporte = pasaporte;
+		this.nacionalidad = nacionalidad;
+	}
+
+
+	public String getNombre() {
+		return nombre;
+	}
+
+
+	public String getPasaporte() {
+		return pasaporte;
+	}
+
+
+	public String getNacionalidad() {
+		return nacionalidad;
+	}
+	
+	
+	
+	
 }
--- Java/Practicas_y_contenido/POO_Aeropuerto/src/Proyecto/Vuelo.java
+++ Java/Practicas_y_contenido/POO_Aeropuerto/src/Proyecto/Vuelo.java
@@ -2,4 +2,62 @@ package Proyecto;
 
 public class Vuelo {
 
+	private String identificador;
+	private String ciudadOrigen;
+	private String ciusdadDestino;
+	private double precio;
+	private int numMaxPasajeros;
+	private int numActualPasajeros;
+	private Pasajero listaPasajeros[];
+	
+	public Vuelo(String identificador, String ciudadOrigen, String ciusdadDestino, double precio, int numMaxPasajeros) {
+		super();
+		this.identificador = identificador;
+		this.ciudadOrigen = ciudadOrigen;
+		this.ciusdadDestino = ciusdadDestino;
+		this.precio = precio;
+		this.numMaxPasajeros = numMaxPasajeros;
+		this.numActualPasajeros = 0;
+		this.listaPasajeros = new Pasajero[numMaxPasajeros];
+		
+	}
+	
+	public void insertarPasajero(Pasajero pasajero) {
+		
+		listaPasajeros[numActualPasajeros] = pasajero;
+		numActualPasajeros++;
+	}
+
+	public String getIdentificador() {
+		return identificador;
+	}
+
+	public String getCiudadOrigen() {
+		return ciudadOrigen;
+	}
+
+	public String getCiusdadDestino() {
+		return ciusdadDestino;
+	}
+
+	public double getPrecio() {
+		return precio;
+	}
+
+	public int getNumMaxPasajeros() {
+		return numMaxPasajeros;
+	}
+
+	public int getNumActualPasajeros() {
+		return numActualPasajeros;
+	}
+
+	public Pasajero getPsajero(int i) {
+		return listaPasajeros[i];
+	}
+	
+
+	
+	
+		
 }
