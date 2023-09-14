7bd913e0a80d1b5edc3a93a75df1ade2d75c7453
pablo segura barranco <89455065+palolo25@users.noreply.github.com>
2023-09-14 11:53:13 +0200

    switch y arreglos

---
+++ JavaScript/switch/ejemplo.js
@@ -0,0 +1,45 @@
+let numeroMes = 9;
+let nombreMes;
+
+switch (numeroMes) {
+    case 1:
+        nombreMes = "Enero";
+        break;
+    case 2:
+        nombreMes = "Febrero";
+        break;
+    case 3:
+        nombreMes = "Marzo";
+        break;
+    case 4:
+        nombreMes = "Abril";
+        break;
+    case 5:
+        nombreMes = "Mayo";
+        break;
+    case 6:
+        nombreMes = "Junio";
+        break;
+    case 7:
+        nombreMes = "Julio";
+        break;
+    case 8:
+        nombreMes = "Agosto";
+        break;
+    case 9:
+        nombreMes = "Septiembre";
+        break;
+    case 10:
+        nombreMes = "Octubre";
+        break;
+    case 11:
+        nombreMes = "Noviembre";
+        break;
+    case 12:
+        nombreMes = "Diciembre";
+        break;
+    default:
+        nombreMes = "Mes inválido";
+}
+
+console.log(`El mes número ${numeroMes} es ${nombreMes}.`);
