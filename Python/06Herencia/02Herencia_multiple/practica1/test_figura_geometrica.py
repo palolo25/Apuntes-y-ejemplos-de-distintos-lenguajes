43212aa6a19a822fd60377858a4754a0f390077d
pablo segura barranco <89455065+palolo25@users.noreply.github.com>
2024-03-19 19:00:39 +0100

    if else

--- Python/06Herencia/02Herencia_multiple/practica1/test_figura_geometrica.py
+++ Python/06Herencia/02Herencia_multiple/practica1/test_figura_geometrica.py
@@ -1,15 +1,19 @@
 from Cuadrado import Cuadrado
 from Rectangulo import Rectangulo
-cuadrado1 = Cuadrado(5,'rojo')
 
-print(f' Calculo del area: {cuadrado1.calcular_area()}')
+
+
+print('Creacion de objeto Cuadrado'.center(50, '-'))
+cuadrado1 = Cuadrado(5,'rojo')
+print(f'Calculo del area: {cuadrado1.calcular_area()}')
 print(cuadrado1)
 
 # MRO - Method Resolution Order
+print('Cuadrado con MRO'.center(50, '-'))
 print(Cuadrado.mro())
 
 # Rectangulo 
-
+print('Creacion de objeto Rectangulo'.center(50, '-'))
 rectangulo1 = Rectangulo(3, 8, 'verde')
 
 print(f'Calculo de area rectangulo: {rectangulo1.calcular_area()}' )
