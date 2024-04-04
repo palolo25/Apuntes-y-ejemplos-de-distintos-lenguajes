d7e77470b56b37f9323e91bd7abef73fb7efda36
pablo segura barranco <89455065+palolo25@users.noreply.github.com>
2024-04-04 15:24:49 +0200

    templates

--- Python/07DJANGO/personas/sap/webapp/views.py
+++ Python/07DJANGO/personas/sap/webapp/views.py
@@ -1,9 +1,10 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 # Create your views here.
 
 def bienvenido(request):
-    return HttpResponse('Hola mundo desde Django')
+    return render(request, 'bienvenido.html')
 
 def despedirse(request):
     return HttpResponse('despedida desde django')
